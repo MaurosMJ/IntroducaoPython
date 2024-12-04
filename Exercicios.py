36c160c1d5d6db86475e419a72f46c333f2fc2b1
Mauros Milach Junior <mauros99milach@gmail.com>
2024-12-04 17:46:53 -0300

    feat: Exercicios.py

--- Exercicios.py
+++ Exercicios.py
@@ -1,3 +1,7 @@
 #Desafio 1: Criar um script que imprima a frase "Olá, mundo Python" na console.
+print("Olá, mundo Python!");
 
-print("Olá, mundo Python!")
+#Desafio 2: Para este desafio, quero que você declare duas variáveis: uma chamada 'nome', que deve guardar o seu primeiro nome como uma string, e outra chamada 'idade', que deve guardar a sua idade como um número inteiro. Depois disso, eu quero que você imprima na tela uma frase que diga 'Olá, meu nome é [nome] e eu tenho [idade] anos'. Lembre-se de substituir [nome] e [idade] pelos valores das variáveis que você declarou.
+nome = "Mauros Milach Junior";
+idade = "25";
+print("Olá, meu nome é " + nome + " e eu tenho " +idade+" anos.")
