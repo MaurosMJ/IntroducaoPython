0dbadbb1ea56a7e345ee2b0813060fca7f9c4d27
Mauros Milach Junior <mauros99milach@gmail.com>
2024-12-05 06:03:04 -0300

    Exercicios.py

--- Exercicios.py
+++ Exercicios.py
@@ -6,3 +6,15 @@ nome = "Mauros Milach Junior";
 idade = "25";
 #print("Olá, meu nome é " + nome + " e eu tenho " +idade+" anos.")
 print("Olá, meu nome é", nome, "e eu tenho", idade, "anos.");
+
+#Desafio 3: Neste desafio, declare duas variáveis: 'num1' que deve guardar o número inteiro 10 e 'num2' que deve guardar o número de ponto flutuante 3,5. Depois disso, realize e imprima na tela o resultado da divisão de 'num1' por 'num2'.
+num1 = 10;
+num2 = 3.5;
+print("Resultado:", num1/num2); 
+
+#Desafio 4: Neste desafio, você deve criar um script que pergunte o nome e a idade do usuário, usando a função input(). Depois disso, o script deve imprimir uma mensagem que diga "Olá, [NOME] você tem [IDADE] anos.".
+print("Qual seu nome?");
+nome = input();
+print("Qual sua idade?");
+idade = input();
+print("Olá,", nome + "! Você tem", idade, "anos.");
